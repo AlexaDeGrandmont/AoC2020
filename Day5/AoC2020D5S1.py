715510c1bef54b89dc3f932b69fb72585644d583
Alexa de Grandmont <35568148+AlexaDeGrandmont@users.noreply.github.com>
2022-07-15 10:28:22 -0400

    Added Day 5

---
+++ Day5/AoC2020D5S1.py
@@ -0,0 +1,32 @@
+input = []
+f = open("puzzleinput.txt", "r")
+for line in f:
+    input.append(line.strip())
+
+def binarySearch(start, end, boardingPass, index, indexStop):
+    while index < indexStop:
+        if boardingPass[index] == 'F' or boardingPass[index] == 'L':
+            end = (start + end) // 2
+            return binarySearch(start, end, boardingPass, index+1, indexStop)  
+        elif boardingPass[index] == 'B' or boardingPass[index] == 'R':
+            start = (start + end) // 2 + 1
+            return binarySearch(start, end, boardingPass, index+1, indexStop)  
+    if start == end:
+        return start
+
+seatIDs = []
+
+for boardingPass in input:
+    row = binarySearch(0, 127, boardingPass, 0, 7)
+    column = binarySearch(0, 7, boardingPass, 7, 10)
+    seatIDs.append((row * 8) + column)
+
+max = 0
+for seatID in seatIDs:
+    if seatID > max:
+        max = seatID
+
+print(max)
+    
+
+
